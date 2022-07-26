027161b43b4db054ebabf49095477b984ccad659
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-26 22:34:49 +0300

    add reports

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Test from './test/test';
+// import ReportPage from 'pages/ReportPage';
 
 export const App = () => {
   return (
     <div>
+      {/* <ReportPage /> */}
       <Routes>
         <Route></Route>
         <Route></Route>
