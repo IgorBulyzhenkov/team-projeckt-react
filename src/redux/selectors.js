78dfcd109ad2fac7a4ede5f126466804a35262b5
Maksym Kundeev <mkundeev@gmail.com>
2022-07-27 10:41:17 +0300

    all request

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,5 +2,4 @@ export const getToken = state => state.currentUser.token;
 export const getRefreshToken = state => state.currentUser.refreshToken;
 export const getSid = state => state.currentUser.sid;
 export const getIsLoggedIn = state => state.currentUser.isLoggedIn;
-export const getName = state => state.currentUser.user.name;
-export const getEmail = state => state.currentUser.user.email;
+export const getEmail = state => state.currentUser.email;
