4f35b33b3881cf4ca1f30fe0b03a5b05ac184699
AnnaLakhmaniyk <anya.21.volynets@gmail.com>
2022-07-27 11:38:36 +0300

    report link

--- src/components/ReportList/ReportList.js
+++ src/components/ReportList/ReportList.js
@@ -1,11 +1,18 @@
 import ReportSvgSelector from './ReportSvgSelector';
+import { FiChevronLeft } from 'react-icons/fi';
+import { FiChevronRight } from 'react-icons/fi';
 import s from './ReportList.module.css';
 function ReportList() {
   return (
     <div>
-      <div>
-        <button type="button">Expenses</button>
-        <button type="button">Income</button>
+      <div className={s.wrapBtn}>
+        <button type="button" className={s.button}>
+          <FiChevronLeft size="25" className={s.arrowBtn} />
+        </button>
+        <p className={s.textBtn}>Expenses</p>
+        <button type="button" className={s.button}>
+          <FiChevronRight size="25" className={s.arrowBtn} />
+        </button>
       </div>
       <ul className={s.list}>
         {ReportSvgSelector.map(({ id, name, image }) => (
