b19282b5d0dbec841308ff0e2cd15fee6c6ef506
Maksym Kundeev <mkundeev@gmail.com>
2022-07-29 01:45:55 +0300

    home rerender fix

--- src/components/Transaction/Transactions.js
+++ src/components/Transaction/Transactions.js
@@ -2,70 +2,47 @@ import { useState, useEffect } from 'react';
 import TransactionHistory from './TransactionHistory';
 import MobileTransaction from './MobileTransaction';
 import s from './Transactions.module.css';
-import {
-  useLazyGetExpenseQuery,
-  useLazyGetIncomeQuery,
-  // useGetIncomeQuery,
-  // useGetExpenseQuery,
-} from '../../redux/kapustaAPI';
+import { useGetIncomeQuery, useGetExpenseQuery } from '../../redux/kapustaAPI';
 
 export default function Transactions() {
-  const [expenses, setExpenses] = useState(true);
+  const [isExpense, setIsExpense] = useState(true);
   const [transactions, setTransactions] = useState([]);
   const [monthStats, setMonthStats] = useState([]);
-  console.log(transactions, '1');
-  // const { data } = useGetExpenseQuery;
-  // const { data } = useGetIncomeQuery;
-  // console.log(data);
-  // useEffect(() => {
 
-  //   if (expenses) {
-  //     setTransactions(expense?.expenses);
-  //     setMonthStats(expense?.monthsStats);
-  //   } else if (income) {
-  //     setTransactions(income?.incomes);
-  //     setMonthStats(income?.monthsStats);
-  //   }
-  // }, [expense, expenses, income]);
-
-  const [getExpense] = useLazyGetExpenseQuery();
-  const [getIncome] = useLazyGetIncomeQuery();
+  const { data: expense } = useGetExpenseQuery();
+  const { data: income } = useGetIncomeQuery;
 
   useEffect(() => {
-    if (expenses) {
-      getExpense()
-        .unwrap()
-        .then(({ expenses, monthsStats }) => {
-          setTransactions([...expenses]);
-          setMonthStats({ ...monthsStats });
-        });
-    } else {
-      getIncome()
-        .unwrap()
-        .then(({ incomes, monthsStats }) => {
-          setTransactions([...incomes]);
-          setMonthStats({ ...monthsStats });
-        });
+    if (isExpense) {
+      setTransactions(expense?.expenses);
+      setMonthStats(expense?.monthsStats);
+    } else if (income) {
+      setTransactions(income?.incomes);
+      setMonthStats(income?.monthsStats);
     }
-  }, [expenses, getExpense, getIncome]);
+  }, [expense, isExpense, income]);
 
   return (
     <div>
       <MobileTransaction />
-      <button className={s.btn} type="button" onClick={() => setExpenses(true)}>
+      <button
+        className={s.btn}
+        type="button"
+        onClick={() => setIsExpense(true)}
+      >
         Expenses
       </button>
       <button
         className={s.btn}
         type="button"
-        onClick={() => setExpenses(false)}
+        onClick={() => setIsExpense(false)}
       >
         Income
       </button>
 
       <div className={s.wrap}>
         <TransactionHistory
-          expenses={expenses}
+          expenses={isExpense}
           transactions={transactions}
           monthStats={monthStats}
         />
