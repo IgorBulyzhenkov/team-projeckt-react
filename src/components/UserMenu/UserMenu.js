a95010062f89294ba071a6f360e2b85a02a7b0a2
Dmytrii Shypilov <95936002+Dmytrii-Shypilov@users.noreply.github.com>
2022-07-28 23:35:27 +0300

    log out modal

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,32 +1,55 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { getEmail,getWidth } from 'redux/selectors'
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { getEmail, getWidth } from 'redux/selectors';
 import { useLogOutUserMutation } from 'redux/kapustaAPI';
 import { resetUser } from 'redux/reducer';
 import { kapustaApi } from 'redux/kapustaAPI';
 import LogoutSharpIcon from '@mui/icons-material/LogoutSharp';
-import Avatar from 'components/Avatar'
-import s from './UserMenu.module.css'
+import Avatar from 'components/Avatar';
+import s from './UserMenu.module.css';
+import ActionModal from 'components/ActionModal';
 
 export default function UserMenu() {
   const dispatch = useDispatch();
   const [logOutUser] = useLogOutUserMutation();
+  const [isModalOpen, setModal] = useState('false');
 
+  const toggleModal = () => {
+    setModal(!isModalOpen);
+  };
 
   const onLogOutUser = () => {
     logOutUser()
       .unwrap()
-      .then(() => {dispatch(resetUser())
-        dispatch(kapustaApi.util.resetApiState())});
+      .then(() => {
+        dispatch(resetUser());
+        dispatch(kapustaApi.util.resetApiState());
+      });
   };
 
-const width = useSelector(getWidth)
-const email = useSelector(getEmail)
-const userName = email?.split('@')[0]
+  const width = useSelector(getWidth);
+  const email = useSelector(getEmail);
+  const userName = email?.split('@')[0];
 
   return (
-    <div className={s.container}>
-      <Avatar/>
-      {width==='tablet'?<><p className={s.userName}>{userName}</p><div className={s.line}></div><div className={s.exitBtn} onClick={onLogOutUser}>Exit</div></>:<LogoutSharpIcon  onClick={onLogOutUser} className={s.icon}/>}
-    </div>
-  )
+    <>
+      <div className={s.container}>
+        <Avatar />
+        {width === 'tablet' ? (
+          <>
+            <p className={s.userName}>{userName}</p>
+            <div className={s.line}></div>
+            <div onClick={toggleModal} className={s.exitBtn} >
+              Exit
+            </div>
+          </>
+        ) : (
+          <LogoutSharpIcon onClick={toggleModal} className={s.icon} />
+        )}
+      </div>
+      {!isModalOpen && <ActionModal toggleModal={toggleModal} logOut={onLogOutUser}>
+        <p className={s.text}>Do you really want to leave?</p>
+      </ActionModal>}
+    </>
+  );
 }
