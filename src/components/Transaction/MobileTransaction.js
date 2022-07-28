a8107ee80a7ddd6275cbcfdde130e75fb35abc48
MaxKorolko <m.v.korolko@gmail.com>
2022-07-28 22:47:23 +0300

    and

--- src/components/Transaction/MobileTransaction.js
+++ src/components/Transaction/MobileTransaction.js
@@ -1,20 +1,34 @@
 import { DeleteOutline } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { useEffect, useState } from 'react';
+import {
+  useGetUserDataQuery,
+  useDeleteTransactionMutation,
+} from '../../redux/kapustaAPI';
+
 import s from './MobileTransaction.module.css';
 
 export default function MobileTransaction() {
-  const transactions = [
-    {
-      description: 'description',
-      category: 'Продукты',
-      amount: 0,
-      date: '2020-12-31',
-      _id: '507f1f77bcf86cd799439013',
-    },
-  ];
+  const [transactions, setTransactions] = useState();
+
+  const { data } = useGetUserDataQuery();
+  const [deleteTransaction] = useDeleteTransactionMutation();
+
+  useEffect(() => {
+    data && setTransactions(data.transactions);
+  }, [data]);
+  // const transactions = [
+  //   {
+  //     description: 'description',
+  //     category: 'Продукты',
+  //     amount: 0,
+  //     date: '2020-12-31',
+  //     _id: '507f1f77bcf86cd799439013',
+  //   },
+  // ];
   return (
     <table className={s.table}>
-      {transactions.map(({ description, category, amount, date, _id }) => {
+      {transactions?.map(({ description, category, amount, date, _id }) => {
         return (
           <tbody key={_id}>
             <tr>
@@ -22,10 +36,14 @@ export default function MobileTransaction() {
                 <span className={s.description}>{description}</span>
               </td>
               <td rowSpan="2" className={s.amount}>
-                {amount}
+                {amount}.00 грн
               </td>
               <td rowSpan="2">
-                <IconButton aria-label="button delete" component="label">
+                <IconButton
+                  onClick={() => deleteTransaction(_id)}
+                  aria-label="button delete"
+                  component="label"
+                >
                   <DeleteOutline />
                 </IconButton>
               </td>
