b778972b1b8cf9d50cf9120c593a16d6adfd7f24
Maksym Kundeev <mkundeev@gmail.com>
2022-07-29 13:15:27 +0300

    google auth

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,13 +1,15 @@
-
-import { useDispatch } from "react-redux";
-import { useRegisterUserMutation, useAuthorizeUserMutation, useLazyAuthGoogleUserQuery,useLazyGetUserDataQuery } from "redux/kapustaAPI";
-import { setUser } from "redux/reducer";
-import s from "./authForm.module.css";
-import googleIcon from "../../img/AuthForm/google-symbol.svg";
+import { useDispatch } from 'react-redux';
+import {
+  useRegisterUserMutation,
+  useAuthorizeUserMutation,
+  useLazyAuthGoogleUserQuery,
+} from 'redux/kapustaAPI';
+import { setUser } from 'redux/reducer';
+import s from './authForm.module.css';
+import googleIcon from '../../img/AuthForm/google-symbol.svg';
 import { toast } from 'react-toastify';
 
-import { useState } from "react";
-
+import { useState } from 'react';
 
 const initialState = {
   email: '',
@@ -20,8 +22,8 @@ const dirtyState = {
 };
 
 const errorState = {
-  emailError: "This is a required field",
-  passwordError: "This is a required field",
+  emailError: 'This is a required field',
+  passwordError: 'This is a required field',
 };
 
 const AuthForm = () => {
@@ -33,89 +35,79 @@ const AuthForm = () => {
   const { emailError, passwordError } = error;
   const { emailDirty, passwordDirty } = dirty;
 
-
   const dispatch = useDispatch();
-
 
   const [registerUser] = useRegisterUserMutation();
   const [authorizeUser] = useAuthorizeUserMutation();
   const [loginGoogle] = useLazyAuthGoogleUserQuery();
 
-  const [getUserData] = useLazyGetUserDataQuery();
-
-  const googleAuth = () => {
-    loginGoogle().then(console.log);
-  };
-  
-  
-
- 
-
-  const onInput = (e) => {
-    setUserForm((prevState) => {
+  const onInput = e => {
+    setUserForm(prevState => {
       return {
         ...prevState,
         [e.target.id]: e.target.value.trim(),
       };
     });
 
-    if (e.target.id === "password") {
+    if (e.target.id === 'password') {
       if (e.target.value.length !== 0 && e.target.value.length < 8) {
-        setError((prevState) => {
+        setError(prevState => {
           return {
             ...prevState,
-            passwordError: "Password must be not less than 8 symbols",
+            passwordError: 'Password must be not less than 8 symbols',
           };
         });
       } else if (!e.target.value) {
-        setError((prevState) => {
+        setError(prevState => {
           return {
             ...prevState,
-            passwordError: "This is a required field",
+            passwordError: 'This is a required field',
           };
         });
       } else {
-        setError((prevState) => {
+        setError(prevState => {
           return {
             ...prevState,
-            passwordError: "",
+            passwordError: '',
           };
         });
       }
     }
 
-    if (e.target.id === "email") {
-      const pattern =  /^[^@\s]+@[^@\s]+\.[^@\s]+$/
+    if (e.target.id === 'email') {
+      const pattern = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
 
-      if (e.target.value) {if (!pattern.test(String(e.target.value).toLowerCase())) {
-          setError(prevState=> {
+      if (e.target.value) {
+        if (!pattern.test(String(e.target.value).toLowerCase())) {
+          setError(prevState => {
             return {
               ...prevState,
-              emailError: "Incorrect email format"
-            }
-          })
+              emailError: 'Incorrect email format',
+            };
+          });
+        } else {
+          setError(prevState => {
+            return {
+              ...prevState,
+              emailError: '',
+            };
+          });
+        }
       } else {
-        setError(prevState=> {
+        setError(prevState => {
           return {
             ...prevState,
-            emailError: ""
-          }
-        })
-      }} else {
-        setError(prevState=> {
-          return {
-            ...prevState,
-            emailError: "This is a required field"
-          }
-        })
+            emailError: 'This is a required field',
+          };
+        });
       }
     }
   };
 
-  const onBlur = (e) => {
+  const onBlur = e => {
     switch (e.target.id) {
-      case "email":
-        setDirty((prevState) => {
+      case 'email':
+        setDirty(prevState => {
           return {
             ...prevState,
             emailDirty: true,
@@ -123,8 +115,8 @@ const AuthForm = () => {
         });
         break;
 
-      case "password":
-        setDirty((prevState) => {
+      case 'password':
+        setDirty(prevState => {
           return {
             ...prevState,
             passwordDirty: true,
@@ -132,45 +124,37 @@ const AuthForm = () => {
         });
         break;
 
-        default: return
+      default:
+        return;
     }
   };
 
-
   const onLogin = () => {
-    if(!user?.email && !user?.password){
-      toast.warn('Please eneter your email and password')
-      return
+    if (!user?.email && !user?.password) {
+      toast.warn('Please eneter your email and password');
+      return;
     }
-    authorizeUser(user).unwrap().then(( data ) => {
-      dispatch(
-        setUser({
-          email: data.userData.email,
-          token: data.accessToken,
-          refreshToken: data.refreshToken,
-          sid: data.sid,
-        })
-      );
-    }).then(() =>
-    getUserData()
+    authorizeUser(user)
       .unwrap()
-      .then(data =>
-       { dispatch(
+      .then(data => {
+        dispatch(
           setUser({
-            email: data.email,
-            balance: data.balance
+            email: data.userData.email,
+            token: data.accessToken,
+            refreshToken: data.refreshToken,
+            sid: data.sid,
+            balance: data.userData.balance,
           })
-        )
-        setUserForm(initialState)}
-      )
-  ).catch(error=>
-    toast.error(error.data.message));
-  }
+        );
+        setUserForm(initialState);
+      })
+      .catch(error => toast.error(error.data.message));
+  };
 
   const onRegister = () => {
-    if(!user?.email && !user?.password){
-      toast.warn('Please eneter email and password for reistration')
-      return
+    if (!user?.email && !user?.password) {
+      toast.warn('Please eneter email and password for reistration');
+      return;
     }
     registerUser(user)
       .unwrap()
@@ -181,29 +165,21 @@ const AuthForm = () => {
             dispatch(
               setUser({
                 email: data.userData.email,
+                balance: data.userData.balance,
                 token: data.accessToken,
                 refreshToken: data.refreshToken,
                 sid: data.sid,
               })
             );
+            setUserForm(initialState);
           })
-      ).then(() =>
-      getUserData()
-        .unwrap()
-        .then(data =>
-          {dispatch(
-            setUser({
-              email: data.email,
-              balance: data.balance
-            })
-          )
-          setUserForm(initialState)}
-        )
-    ).catch(error=>
-      toast.error(error.data.message));
-  }
+      )
+      .catch(error => toast.error(error.data.message));
+  };
+  const googleAuth = () => {
+    loginGoogle().then(console.log);
+  };
 
-  
   return (
     <form className={s.form}>
       <p className={s.text}>You can log in with your Google Account:</p>
@@ -211,6 +187,11 @@ const AuthForm = () => {
         <div className={s.google}>
           <img className={s.icon} src={googleIcon} alt="" />
           <span className={s.iconText}>Google</span>
+          <a
+            className={s.link}
+            onClick={googleAuth}
+            href="https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&client_id=665888736356-aq6fvfmau6mupt4nfbms5tfch0u2698i.apps.googleusercontent.com&prompt=consent&redirect_uri=https%3A%2F%2Fkapusta-backend.goit.global%2Fauth%2Fgoogle-redirect&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile"
+          ></a>
         </div>
       </div>
 
