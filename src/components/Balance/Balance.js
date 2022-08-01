23333959d49a686852f938ec2c0b606082e528fc
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-08-01 11:53:37 +0300

    log

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -14,7 +14,6 @@ import NumberFormat from 'react-number-format';
 const Balance = () => {
   const [setBalanceValue] = useChangeBalanceMutation();
   const { data, isFetching } = useGetUserDataQuery();
-  console.log(isFetching);
 
   const hasBalance = !!data?.balance;
   const hasTransactions = data?.transactions.length > 0;
