801271b4c511459a713725ff802b3c3dd7a7d9ca
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-08-03 16:19:03 +0300

    final scroll chart bar

--- src/components/Report/ReportGraph/ReportGraph.js
+++ src/components/Report/ReportGraph/ReportGraph.js
@@ -8,7 +8,7 @@ import {
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { Bar } from 'react-chartjs-2';
 import { useSelector } from 'react-redux';
-import { useContext } from 'react';
+import { useContext, useRef } from 'react';
 import { ThemeContext } from 'components/App';
 import { darkThemeStyles } from 'services/theme-styles';
 import { getWidth, getCategory } from '../../../redux/selectors';
@@ -16,6 +16,7 @@ import { getWidth, getCategory } from '../../../redux/selectors';
 ChartJS.register(BarElement, LinearScale, CategoryScale, ChartDataLabels);
 
 function ReportGraph({ data }) {
+  const chartRef = useRef();
   const screen = useSelector(getWidth);
   const category = useSelector(getCategory);
   const newData = data[category];
@@ -90,7 +91,6 @@ function ReportGraph({ data }) {
     }
   };
 
-  
   const gridLineY = () => {
     switch (screen) {
       case 'tablet':
@@ -106,7 +106,7 @@ function ReportGraph({ data }) {
   const paddingScreenRight = () => {
     switch (screen) {
       case 'tablet':
-        return 0;
+        return 15;
 
       case 'mobile':
         return 80;
@@ -157,11 +157,136 @@ function ReportGraph({ data }) {
     }
   };
 
+  const colorFill = themeColor === 'dark' ? 'rgb(63, 78, 79)' : '#ffffff';
+
+  // const colorBorder = themeColor === 'dark' ? 'transparent' : 'rgb(63, 78, 79)';
+
+  const minX = screen === 'tablet' ? 0 : null;
+  const maxX = screen === 'tablet' ? 6 : null;
+
+  const myChart = {
+    id: 'myChart',
+
+    afterEvent(chart, args) {
+      const {
+        canvas,
+        chartArea: { left, right, height, top},
+      } = chart;
+
+      canvas.addEventListener('mousemove', event => {
+        const x = args.event.x;
+        const y = args.event.y;
+
+        if (
+          x >= left - 12 &&
+          x <= left + 12 &&
+          y >= height / 2 + top - 15 &&
+          y <= height / 2 + top + 32
+        ) {
+          return (canvas.style.cursor = 'pointer');
+        } else if (
+          x >= right - 12 &&
+          x <= right + 12 &&
+          y >= height / 2 + top - 15 &&
+          y <= height / 2 + top + 32
+        ) {
+          return (canvas.style.cursor = 'pointer');
+        } else {
+          return (canvas.style.cursor = 'default');
+        }
+      });
+    },
+
+    afterDraw(chart) {
+      const {
+        ctx,
+        chartArea: { left, right, height, top},
+      } = chart;
+
+      const angle = Math.PI / 180;
+
+      class circleChevron {
+        draw(ctx, x1, pixel) {
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'transparent';
+          ctx.arc(x1, height / 2 + top, 15, angle * 0, angle * 360, false);
+          ctx.stroke();
+          ctx.fill();
+          ctx.fillStyle = colorFill;
+          ctx.closePath();
+
+          ctx.beginPath();
+          ctx.lineWidth = 2;
+          ctx.strokeStyle = 'rgb(255, 117, 29)';
+          ctx.moveTo(x1 + pixel, height / 2 + top - 7.5);
+          ctx.lineTo(x1 - pixel, height / 2 + top);
+          ctx.lineTo(x1 + pixel, height / 2 + top + 7.5);
+          ctx.stroke();
+          ctx.closePath();
+        }
+      }
+
+      let drawCircleLeft = new circleChevron();
+      drawCircleLeft.draw(ctx, left, 5);
+
+      let drawCircleRight = new circleChevron();
+      drawCircleRight.draw(ctx, right, -5);
+    },
+  };
+
+  function moveScroll(event) {
+    const { current: chart } = chartRef;
+    const {
+      canvas,
+      chartArea: { left, right, top, height },
+    } = chart;
+
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    if (
+      x >= left - 12 &&
+      x <= left + 12 &&
+      y >= height / 2 + top - 15 &&
+      y <= height / 2 + top + 32
+    ) {
+      chart.options.scales.x.min = chart.options.scales.x.min - 7;
+      chart.options.scales.x.max = chart.options.scales.x.max - 7;
+
+      if (chart.options.scales.x.max <= 0) {
+        chart.options.scales.x.min = 0;
+        chart.options.scales.x.max = 6;
+      }
+    }
+
+    if (
+      x >= right - 12 &&
+      x <= right + 12 &&
+      y >= height / 2 + top - 15 &&
+      y <= height / 2 + top + 32
+    ) {
+      chart.options.scales.x.min = chart.options.scales.x.min + 7;
+      chart.options.scales.x.max = chart.options.scales.x.max + 7;
+
+      if (chart.options.scales.x.max >= dataChart.datasets[0].data.length) {
+        chart.options.scales.x.min = dataChart.datasets[0].data.length - 7;
+        chart.options.scales.x.max = dataChart.datasets[0].data.length;
+      }
+    }
+    chart.update();
+  }
+
+  const pluginsChart = screen === 'tablet' ? myChart : [];
+
   return (
     <div className={s.wrap} style={MobileTheme}>
       <div className={s.container}>
         <Bar
           data={dataChart}
+          ref={chartRef}
+          onClick={moveScroll}
           options={{
             maintainAspectRatio: false,
             responsive: true,
@@ -170,6 +295,7 @@ function ReportGraph({ data }) {
               padding: {
                 top: 30,
                 right: paddingScreenRight(),
+                left: 12,
               },
             },
             plugins: {
@@ -183,6 +309,8 @@ function ReportGraph({ data }) {
             },
             scales: {
               x: {
+                min: minX,
+                max: maxX,
                 ticks: {
                   display: lineX(),
                   color: textColor,
@@ -207,6 +335,7 @@ function ReportGraph({ data }) {
               },
             },
           }}
+          plugins={[pluginsChart]}
           className={s.bar}
         />
       </div>
