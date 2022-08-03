3a2a2c61744ff2a7bdeb2d3a96a3017632cb696c
Maksym Kundeev <mkundeev@gmail.com>
2022-08-03 20:06:18 +0300

    done

--- src/components/FormAddExpense/FormAddExpense.js
+++ src/components/FormAddExpense/FormAddExpense.js
@@ -279,6 +279,7 @@ const FormAddExpense = ({ expense, handleClick }) => {
           </div>
           <div className={s.notificationWraps}>
             <input
+              autocomplete="off"
               type="text"
               id="description"
               name="description"
@@ -311,6 +312,7 @@ const FormAddExpense = ({ expense, handleClick }) => {
           <div className={s.currencyWrapp}>
             <div className={s.notificationWraps}>
               <NumberFormat
+                autocomplete="off"
                 allowNegative={false}
                 suffix={VpWidth === 'mobile' ? ' UAH' : ''}
                 decimalScale={2}
